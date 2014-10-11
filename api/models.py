b2800234c8a03dda01ce64d404bb711d7a883e47
Shang Yuanchun <shangyuanchun@baidu.com>
2014-10-11 12:24:06 +0800

    add Comment

--- api/models.py
+++ api/models.py
@@ -1,2 +1,13 @@
 from django.db import models
 
+class Comment(models.Model):
+
+    comment_id = models.AutoField(primary_key=True)
+    user_id = models.IntegerField(default=0)
+    comment_type = models.IntegerField('1: normal comment, 2: reply', default=1)
+    comment_date = models.DateTimeField('date published')
+    comment_content = models.TextField(max_length=4096)
+    comment_ups = models.IntegerField(default=0)
+    comment_downs = models.IntegerField(default=0)
+    comment_parent = models.BigIntegerField(default=0)
+
