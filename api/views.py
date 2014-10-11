2d2a3f547d79b6c00a4b31f5a32e37ffbfbb291f
Shang Yuanchun <shangyuanchun@baidu.com>
2014-10-11 10:08:06 +0800

    add files

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
+# -*- encoding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import JsonResponse
 
-# Create your views here.
-
 def index(request):
-    return JsonResponse(data={})
+    return JsonResponse(data = {'我们': '呵呵'}, content_type = 'application/json; charset=utf-8')
--- comment/urls.py
+++ comment/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'comment.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'comment.views.index', name='index'),
     url(r'^comment/', include('api.urls')),
 )
