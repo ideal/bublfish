fb1e10bb50c2c80ce0aabbecb1ef7069fd867fe2
Shang Yuanchun <shangyuanchun@baidu.com>
2014-10-11 15:31:39 +0800

    change response

--- api/response.py
+++ api/response.py
@@ -16,9 +16,11 @@ CONTENT_TYPE_JSON = 'application/json; charset=utf-8'
 KWARGS_JSON = {'content_type': CONTENT_TYPE_JSON}
 
 def error(status_code, status_msg, is_json=True):
+    if not is_json:
+        return HttpResponse(content=status_msg, status=status_code)
+
     data = DATA_ERR
     data['status'] = status_code
     data['info']   = status_msg
-    return (JsonResponse(data, **KWARGS_JSON) if is_json
-           else HttpResponse(content=status_msg, status=status_code))
+    return JsonResponse(data, **KWARGS_JSON)
 
